e62d40cadd43fd293e67a1b679d0b12547e90889
Ekansh Pali <ekanshpali@gmail.com>
2021-12-12 16:17:59 +0530

    Changed config and modified Encoder

--- FlatBufferEncoder/src/FlatBufferEncoder.cpp
+++ FlatBufferEncoder/src/FlatBufferEncoder.cpp
@@ -5,11 +5,7 @@
 using namespace Data;
 int main()
 {
-	std::string path;
-	std::cout << "Give the path of the folder where the .bin file should be created! : ";
-	std::cin >> path;
-	std::cout << std::endl;
-	path += "\fb_bytes.bin";
+
 	std::cout << "Press 1 to encode client as person : Press any other key to encode client as group ";
 	char input = getchar();
 	flatbuffers::FlatBufferBuilder builder(1024);
@@ -33,14 +29,14 @@ int main()
 		auto nameslist = builder.CreateVectorOfStrings(Names);
 		auto clientgroup = CreateGroup(builder, groupname, averageAge, averageWeight, nameslist);
 		auto client = CreateClient(builder, ClientType::ClientType_Group, clientgroup.Union());
-
+		builder.Finish(client);
 	}
 	
 	
 
 	uint8_t *buf = builder.GetBufferPointer();
 	std::ofstream Flatostream;
-	Flatostream.open(path, std::ios::out | std::ios::binary);
+	Flatostream.open("fb_bytes.bin", std::ios::out | std::ios::binary);
 	int size = builder.GetSize();
 	Flatostream.write((const char*)buf, size);
 	Flatostream.close();
